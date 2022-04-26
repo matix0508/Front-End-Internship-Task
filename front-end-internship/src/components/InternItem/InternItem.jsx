9d02af38eb75a6a78a19d48c49891e1ca4662351
Mateusz Wlekliński <wleklinskimateusz05@gmail.com>
2022-04-26 21:25:56 +0200

    edit icon

--- front-end-internship/src/components/InternItem/InternItem.jsx
+++ front-end-internship/src/components/InternItem/InternItem.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./InternItem.module.scss";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 export const InternItem = ({ intern }) => {
+  
   return (
     <div className={styles.InternItem}>
-      <div className={styles.InternItem__name}>{intern.name}</div>
-      <div className={styles.InternItem__edit}><NavLink to={`/interns/${intern.id}`}>Edit</NavLink></div>
+      <div className={styles.InternItem__name}> {intern.name} </div>{" "}
+      <div className={styles.InternItem__edit}>
+        {" "}
+        <NavLink to={`/interns/${intern.id}`}> <FontAwesomeIcon icon={faPen} color={styles.InternItem__edit.color} /> </NavLink>
+      </div>
     </div>
   );
 };
