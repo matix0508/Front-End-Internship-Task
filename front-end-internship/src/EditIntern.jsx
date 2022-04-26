42ff45df2d99ec0d6f347a5b4e40afd4a091f91c
Mateusz Wlekliński <wleklinskimateusz05@gmail.com>
2022-04-26 14:35:52 +0200

    apply fetch data from server

--- front-end-internship/src/EditIntern.jsx
+++ front-end-internship/src/EditIntern.jsx
@@ -1,28 +1,50 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { useQuery } from "react-query";
+import { useParams } from "react-router";
+import { NavLink } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const EditIntern = () => {
-    const { id } = useParams();
+  const { id } = useParams();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
 
-    useEffect(() => {
-        //TODO: get intern from REST api http://localhost:3001/interns/:id
-        console.log(`I want to get intern with id: ${id}!`)
-    }, [id]);
+  const { data, isLoading, isError } = useQuery(`intern${id}`, () =>
+    fetch(`http://localhost:3001/interns/${id}`).then((res) => res.json())
+  );
 
-    return (
-        <div>
-            <NavLink to="/">Back to list </NavLink>
-            <form>
-                <label>Name</label>
-                <input type="text" name="name" />              
-                <label>Email</label>
-                <input type="text" name="email" />
-                
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
-    );
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (isError) {
+    return <p>Error :(</p>;
+  }
+  if (!data) {
+    return <p>No data :/</p>;
+  }
+
+  // useEffect(() => {
+  //     //TODO: get intern from REST api http://localhost:3001/interns/:id
+  //     console.log(`I want to get intern with id: ${id}!`)
+  // }, [id]);
+
+  return (
+    <div>
+      <NavLink to="/"> Back to list </NavLink>{" "}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label> Name </label>{" "}
+        <input defaultValue={data.name} {...register("name")} />
+        <label> Email </label>{" "}
+        <input defaultValue={data.email} {...register("email")} />
+        <input type="submit" value="Submit" />
+      </form>{" "}
+    </div>
+  );
 };
 
 export default EditIntern;
--- front-end-internship/src/index.jsx
+++ front-end-internship/src/index.jsx
@@ -3,14 +3,19 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider, QueryClient } from "react-query";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>
 );
